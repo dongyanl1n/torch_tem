7890d7a4b27eeb6c3c89feb0357b50fbe950d6ee
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2022-07-30 16:08:19 -0400

    small fix of paths to work on Mila cluster

--- test.py
+++ test.py
@@ -22,28 +22,28 @@ np.random.seed(0)
 torch.manual_seed(0)
 
 # Choose which trained model to load
-date = '2020-10-19' # 2020-10-13 run 0 for successful node agent
+date = '2022-07-30' # 2020-10-13 run 0 for successful node agent
 run = '0'
-index = '32000'
-
+index = '1999'
+breakpoint()
 # Load the model: use import library to import module from specified path
-model_spec = importlib.util.spec_from_file_location("model", '../Summaries/' + date + '/run' + run + '/script/model.py')
+model_spec = importlib.util.spec_from_file_location("model", './Summaries/' + date + '/run' + run + '/script/model.py')
 model = importlib.util.module_from_spec(model_spec)
 model_spec.loader.exec_module(model)
 
 # Load the parameters of the model
-params = torch.load('../Summaries/' + date + '/run' + run + '/model/params_' + index + '.pt')
+params = torch.load('./Summaries/' + date + '/run' + run + '/model/params_' + index + '.pt')
 # Create a new tem model with the loaded parameters
 tem = model.Model(params)
 # Load the model weights after training
-model_weights = torch.load('../Summaries/' + date + '/run' + run + '/model/tem_' + index + '.pt')
+model_weights = torch.load('./Summaries/' + date + '/run' + run + '/model/tem_' + index + '.pt')
 # Set the model weights to the loaded trained model weights
 tem.load_state_dict(model_weights)
 # Make sure model is in evaluate mode (not crucial because it doesn't currently use dropout or batchnorm layers)
 tem.eval()
 
 # Make list of all the environments that this model was trained on
-envs = list(glob.iglob('../Summaries/' + date + '/run' + run + '/script/envs/*'))
+envs = list(glob.iglob('./Summaries/' + date + '/run' + run + '/script/envs/*'))
 # Set which environments will include shiny objects
 shiny_envs = [False, False, True, True]
 # Set the number of walks to execute in parallel (batch size)
