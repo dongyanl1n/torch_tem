6a584b7f1fb6d2ce8d0fc38c0144e6f4a5ff4ebc
lindongy <lindongy@login-2.server.mila.quebec>
2022-09-10 17:47:05 -0400

    update submist sh script

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=rnn_simple
+#SBATCH --job-name=og_mlp_1
 #SBATCH --ntasks=1
 #SBATCH --partition=long
 #SBATCH --gres=gpu:rtx8000:1
@@ -12,4 +12,6 @@
 module load anaconda/3 python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.7.0
 source $HOME/testenv/bin/activate
 
-python python rl_run_simple.py --size 5 --agent_type 'rnn' --save_dir 'experiments/2022-09-01/0' --num_episodes 20000 --save_model_freq 10000 --window_size 2000 --lr 0.0001 --num_neurons 512
+#python rl_run_simple.py --size 5 --agent_type 'mlp' --save_dir 'experiments/2022-09-03/1' --load_existing_agent 'experiments/2022-09-01/1/baseline_mlp_Epi49999.pt' --num_episodes 100000 --save_model_freq 50000 --window_size 10000 --lr 0.00008 --num_neurons 512
+
+python rl_run_simple.py --size 5 --agent_type 'og_mlp' --save_dir 'experiments/2022-09-03/og/1' --num_episodes 100000 --save_model_freq 20000 --window_size 10000 --lr 0.0005 --num_neurons 256 --load_existing_agent "experiments/2022-09-03/og/1/baseline_mlp_Epi49999.pt" --record_activity True
