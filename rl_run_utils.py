24afb1be49e848767405c94eea64ba8a9bad4383
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2022-09-10 22:21:25 -0400

    Can run SimpleNav with visual input

--- rl_run_utils.py
+++ rl_run_utils.py
@@ -28,19 +28,16 @@ def bin_rewards(epi_rewards, window_size):
     return avg_rewards
 
 
-def train_neural_net_on_SimpleNavigation(env, agent, optimizer, num_episodes, save_model_freq, add_input, mode, save_dir, agent_type, record_activity=False):
-    assert mode in ['tem', 'baseline']
+def train_neural_net_on_SimpleNavigation(env, agent, optimizer, num_episodes, save_model_freq, save_dir, agent_type, record_activity=False):
     steps_taken = []
     if record_activity:
-        assert agent_type == "rnn" or agent_type == "mlp"
-        assert len(agent.hidden_types) == 2  # Two layer MLP for now
         trial_counter = []
         location = []
         action = []
-        if agent_type == "mlp":
+        if agent_type == "mlp" or agent_type == 'conv_mlp':
             lin_1_activity = []
             lin_2_activity = []
-        elif agent_type == "rnn":
+        elif agent_type == "rnn" or agent_type == 'conv_rnn':
             hx_activity = []
             cx_activity = []
             lin_activity = []
@@ -53,31 +50,26 @@ def train_neural_net_on_SimpleNavigation(env, agent, optimizer, num_episodes, sa
         target_loc[i_episode] = observation["target"]
         agent.reinit_hid()
         while not done: # act, step
-            if mode == 'tem':
-                assert add_input is not None
-                input_to_model = torch.unsqueeze(torch.unsqueeze(torch.as_tensor(np.concatenate((add_input, np.concatenate(list(observation.values()))))), dim=0), dim=1).float()
-            elif mode == 'baseline':
-                assert add_input is None
-                if agent_type == "rnn" or agent_type == "mlp":
-                    input_to_model = torch.unsqueeze(torch.unsqueeze(torch.as_tensor(np.concatenate(list(observation.values()))), dim=0), dim=1).float()[0]
-                else:
-                    input_to_model = torch.unsqueeze(torch.unsqueeze(torch.as_tensor(np.concatenate(list(observation.values()))), dim=0), dim=1).float()
+            if agent_type == "rnn" or agent_type == "mlp":
+                input_to_model = torch.unsqueeze(torch.unsqueeze(torch.as_tensor(np.concatenate(list(observation.values()))), dim=0), dim=1).float()[0]
+            elif agent_type == "conv_rnn" or agent_type == "conv_mlp":
+                input_to_model = torch.unsqueeze(torch.Tensor(np.reshape(render_observation_as_image(
+                    env.size, observation["agent"], observation["target"], show_target=True), (3, env.size, env.size))), dim=0).float()
             pol, val = agent.forward(input_to_model)
             act, p, v = select_action(agent, pol, val)
             if record_activity:
                 action.append(act)
                 trial_counter.append(i_episode)
                 location.append(observation["agent"])
-                if agent_type == "mlp":
-                    lin_1_activity.append(agent.cell_out[0].clone().detach().cpu().numpy().squeeze())
-                    lin_2_activity.append(agent.cell_out[1].clone().detach().cpu().numpy().squeeze())
-                elif agent_type == "rnn":
-                    hx_activity.append(agent.hx[0].clone().detach().cpu().numpy().squeeze())
-                    cx_activity.append(agent.cx[0].clone().detach().cpu().numpy().squeeze())
-                    lin_activity.append(agent.cell_out[1].clone().detach().cpu().numpy().squeeze())
+                if agent_type == "mlp" or agent_type == "conv_mlp":
+                    lin_1_activity.append(agent.cell_out[agent.hidden_types.index("linear")].clone().detach().cpu().numpy().squeeze())
+                    lin_2_activity.append(agent.cell_out[agent.hidden_types.index("linear")+1].clone().detach().cpu().numpy().squeeze())
+                elif agent_type == "rnn" or agent_type == "conv_rnn":
+                    hx_activity.append(agent.hx[agent.hidden_types.index("lstm")].clone().detach().cpu().numpy().squeeze())
+                    cx_activity.append(agent.cx[agent.hidden_types.index("lstm")].clone().detach().cpu().numpy().squeeze())
+                    lin_activity.append(agent.cell_out[agent.hidden_types.index("lstm")+1].clone().detach().cpu().numpy().squeeze())
             observation, reward, done, info = env.step(act)
             agent.rewards.append(reward)
-        # print(f"This episode has {len(agent.rewards)} steps")
         steps_taken.append(len(agent.rewards))
         p_loss, v_loss = finish_trial(agent, 0.99, optimizer)
         if (i_episode + 1) % save_model_freq == 0:
@@ -87,7 +79,7 @@ def train_neural_net_on_SimpleNavigation(env, agent, optimizer, num_episodes, sa
                 'optimizer_state_dict': optimizer.state_dict(),
                 'p_loss': p_loss,
                 'v_loss': v_loss
-            }, os.path.join(save_dir, f'{mode}_{agent_type}_Epi{i_episode}.pt'))
+            }, os.path.join(save_dir, f'{agent_type}_Epi{i_episode}.pt'))
     if record_activity:  # zip recorded data
         if agent_type == "mlp":
             data = {"init_loc": init_loc,
@@ -112,20 +104,17 @@ def train_neural_net_on_SimpleNavigation(env, agent, optimizer, num_episodes, sa
     return steps_taken, data
 
 
-def train_neural_net_on_MorrisWaterMaze(env, agent, optimizer, num_episodes, save_model_freq, add_input, mode, save_dir, agent_type, num_episodes_per_block=2, record_activity=False):
-    assert mode in ['tem', 'baseline']
+def train_neural_net_on_MorrisWaterMaze(env, agent, optimizer, num_episodes, save_model_freq, save_dir, agent_type, num_episodes_per_block=2, record_activity=False):
     assert isinstance(agent, AC_Conv_Net), "agent has to have convolutional component because images are used as inputs here"
     steps_taken = []
     if record_activity:
-        assert agent_type == "rnn" or agent_type == "mlp"
-        assert len(agent.hidden_types) == 2  # Two layer MLP for now
         trial_counter = []
         location = []
         action = []
-        if agent_type == "mlp":
+        if agent_type == "conv_mlp":
             lin_1_activity = []
             lin_2_activity = []
-        elif agent_type == "rnn":
+        elif agent_type == "conv_rnn":
             hx_activity = []
             cx_activity = []
             lin_activity = []
@@ -172,7 +161,7 @@ def train_neural_net_on_MorrisWaterMaze(env, agent, optimizer, num_episodes, sav
                 'optimizer_state_dict': optimizer.state_dict(),
                 'p_loss': p_loss,
                 'v_loss': v_loss
-            }, os.path.join(save_dir, f'{mode}_{agent_type}_Epi{i_episode}.pt'))
+            }, os.path.join(save_dir, f'{agent_type}_Epi{i_episode}.pt'))
     if record_activity:  # zip recorded data
         if agent_type == "mlp":
             data = {"init_loc": init_loc,
