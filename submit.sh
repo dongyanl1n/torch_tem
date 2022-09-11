c205ae800babc49cab277b1a2886081c041616ff
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2022-09-10 22:25:18 -0400

    to submit simplenav with visual input

--- submit.sh
+++ submit.sh
@@ -12,6 +12,6 @@
 module load anaconda/3 python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.7.0
 source $HOME/testenv/bin/activate
 
-#python rl_run_simple.py --size 5 --agent_type 'mlp' --save_dir 'experiments/2022-09-03/1' --load_existing_agent 'experiments/2022-09-01/1/baseline_mlp_Epi49999.pt' --num_episodes 100000 --save_model_freq 50000 --window_size 10000 --lr 0.00008 --num_neurons 512
+#python rl_run.py --size 5 --agent_type 'mlp' --save_dir 'experiments/2022-09-03/og/1' --num_episodes 100000 --save_model_freq 20000 --window_size 10000 --lr 0.0005 --num_neurons 256 --load_existing_agent "experiments/2022-09-03/og/1/baseline_mlp_Epi49999.pt" --record_activity True
 
-python rl_run_simple.py --size 5 --agent_type 'og_mlp' --save_dir 'experiments/2022-09-03/og/1' --num_episodes 100000 --save_model_freq 20000 --window_size 10000 --lr 0.0005 --num_neurons 256 --load_existing_agent "experiments/2022-09-03/og/1/baseline_mlp_Epi49999.pt" --record_activity True
+python rl_run.py --size 5 --num_episodes 50000 --lr 0.0005 --window_size 5000 --agent_type 'conv_rnn' --save_dir "experiments/2022-09-10" --save_model_freq 25000 --record_activity False --morris_water_maze False
